a029790434765f8866eafaf83c0106ed8072e1e6
Ignacio <rogerpeke97@gmail.com>
2021-04-03 03:04:00 -0300

    Tweaked particle scene

--- src/components/three.js
+++ src/components/three.js
@@ -54,9 +54,7 @@ const style = {
         transition: "all 0.1s ease-out",
         top: "5%",
         zIndex: "2",
-        backgroundColor: "black",
-        opacity: "0",
-        boxShadow: "0 0 34px 18px grey"
+        backgroundColor: "transparent",
     },
     my_work_title: {
         display: "grid",
@@ -298,7 +296,7 @@ function ThreeJsScene() {
     useEffect(() => {
         if (componentLoaded === false) {
             let scene = new THREE.Scene();
-            let manager = new THREE.LoadingManager(); // WHEN MODELS ARE LOADED .onLoad will be called
+            let manager = new THREE.LoadingManager();
             camera.current = new THREE.PerspectiveCamera(75, canvasContainer.current.clientWidth / canvasContainer.current.clientHeight, 0.1, 1000);
             let renderer = new THREE.WebGLRenderer();
             let height = canvasContainer.current.clientHeight;
@@ -307,18 +305,31 @@ function ThreeJsScene() {
 
 
             for(let i = -15; i < 15; i+=5){
-                const light = new THREE.PointLight( 0xffffff, 0.5, 5000 );
+                const light = new THREE.PointLight( 0xffffff, 0.8, 500 );
                 light.position.set( i, 8, -20 );
-                light.castShadow = true; // default false
-                //Set up shadow properties for the light
-                light.shadow.mapSize.width = 512; // default
-                light.shadow.mapSize.height = 512; // default
+                light.castShadow = true;
+                light.shadow.mapSize.width = 512; 
+                light.shadow.mapSize.height = 512;
                 scene.add( light);
             }
             
+            /*const plane_geometry = new THREE.PlaneGeometry( 40, 10, 32 );
+
+            const plane_material = new THREE.MeshPhongMaterial({opacity: 0.5, color: 'black', transparent: true});
+
+            const plane = new THREE.Mesh(plane_geometry, plane_material);
+
+            plane.rotateX( - Math.PI / 2 );
+
+            plane.position.y = -0.3;
+
+            plane.position.z = 0;
 
 
-            const geometry = new THREE.PlaneGeometry( 40, 15, 32 );
+            scene.add(plane);*/
+
+
+            const geometry = new THREE.PlaneGeometry( 40, 10, 32 );
 
             let groundMirror = new Reflector( geometry, {
                 clipBias: 0.003,
@@ -327,7 +338,7 @@ function ThreeJsScene() {
                 color: 0x000000,
                 opacity: 0.1
             } );
-            groundMirror.position.y = -0.3;
+            groundMirror.position.y = -0.35;
             groundMirror.position.z = 0;
             groundMirror.rotateX( - Math.PI / 2 );
             scene.add( groundMirror );
@@ -340,7 +351,7 @@ function ThreeJsScene() {
             camera
             .current.rotation.y = 0;
             camera
-            .current.rotation.x = -0.5;
+            .current.rotation.x = -0.8;
             camera
             .current.rotation.z = 0;
 
@@ -412,7 +423,7 @@ function ThreeJsScene() {
                 transparent: false
             });
 
-            const planeSize = 0.05;
+            /*const planeSize = 0.05;
 
             const shadowTexture = loader.load('roundshadow.png');
 
@@ -420,26 +431,26 @@ function ThreeJsScene() {
             const shadowMat = new THREE.MeshPhongMaterial({
                 map: shadowTexture, opacity: 4, alphaTest: 0.1,
                 blending: THREE.CustomBlending, color: 'black',
-                transparent: false,
-            });
+                transparent: true,
+            });*/
             let positions = [];
-            let shadows = [];
-            for (let i = 5; i > 0; i-= 0.2) {
-                let count = -10;
-                for(let j = 0; j < 20; j+=0.2){
+            //let shadows = [];
+            for (let i = 5; i > 1; i-= 0.2) {
+                let count = -5;
+                for(let j = 0; j < 10; j+=0.2){
                     let posX = count;
                     let posY = 0;
                     let posZ = i;
                     positions.push(posX, posY, posZ);
 
-                    let shadowMesh = new THREE.Mesh(shadowGeo, shadowMat);
+                   /* let shadowMesh = new THREE.Mesh(shadowGeo, shadowMat);
 
                     shadowMesh.position.x = count;
                     shadowMesh.position.y = -0.2;
                     shadowMesh.position.z = i;  
                     shadowMesh.rotation.x = Math.PI * -.5;
                     shadows.push(shadowMesh);
-                    scene.add(shadowMesh);
+                    scene.add(shadowMesh);REMOVING SHADOWS BUT LEAVING THEM COMMENTED AS THEY CAN BE USEFUL FOR ANOTHER PROJECT*/
                     count+=0.2;   
                 }
             }
@@ -458,9 +469,9 @@ function ThreeJsScene() {
                     particle_max_position_wave = 0.5 * Math.sin(((2 * Math.PI) / 2.5) * star[i+1] - ((2 * Math.PI) / period * time));
 
                     star[i] += (particle_max_position_wave / 100);
-                    if(i < 2525){
+                    /*if(i < 1020){
                         shadows[i-1].material.opacity -= particle_max_position_wave / 1000;
-                    }
+                    }*/
                     particleSys.geometry.attributes.position.needsUpdate = true;
                     time+=0.001;
                 }
@@ -730,7 +741,7 @@ function ThreeJsScene() {
         let y = canvasContainer.current.getBoundingClientRect().height / 2 - mousey ;
         transparent_overlay.current.style.transform = `perspective(700px) rotateY(${x / 100}deg) rotateX(${ y / 100}deg)`;
         camera.current.rotation.y = (x / 100) * (Math.PI / 180);
-        camera.current.rotation.x = -(y / 100) * (Math.PI / 180) - 0.5;
+        camera.current.rotation.x = -(y / 100) * (Math.PI / 180) - 0.8;
     }
 
     return (
@@ -759,7 +770,9 @@ function ThreeJsScene() {
                 onMouseMove={(event) => {
                 mouseMove(event);
             }}>
-                <div ref={transparent_overlay} style={style.transparent_overlay} onMouseMove={(e)=>mouseMove(e)}></div>
+                <div ref={transparent_overlay} style={style.transparent_overlay} onMouseMove={(e)=>mouseMove(e)}>
+                    <div>Ignacio Martin Diaz</div>
+                </div>
             </div>
 
             <div className= "THEFREAKINGPAGE" style={{display: "grid", maxWidth: "100%", minHeight: "1620px", maxHeight: "1620px", position: "relative",
