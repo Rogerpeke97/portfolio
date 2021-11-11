dd0296fcc2a9d0dbb0f024dc0ab6630719dfc6e3
Ignacio <rogerpeke97@gmail.com>
2021-11-10 21:36:28 -0500

    Removed ugly presentation

--- src/views/Home.js
+++ src/views/Home.js
@@ -11,31 +11,30 @@ function Home() {
 	const commercialSection = useRef(0);
 	const projectsSection = useRef(0);
 
-	const {mediaQuery} = useContext(MediaContext)
+	const { mediaQuery } = useContext(MediaContext)
 
-	function themeHandler(){
+	function themeHandler() {
 		// console.log(darkMode, mediaQuery)
 	}
 
+	const welcomeMessage = 'Get in touch!'.split("")
+
 	return (
 		<div>
-			<NavBar themeHandler={()=>themeHandler()} />
-			<Waves mediaQuery={mediaQuery}/>
+			<NavBar themeHandler={() => themeHandler()} />
+			<Waves mediaQuery={mediaQuery} />
 			<div className="grid sections-home">
 				<Particles div={projectsSection} colorParticles={"blue"} />
 				<Projects mediaQuery={mediaQuery} projectsSection={projectsSection} />
 			</div>
 			<div className="grid sections-home" ref={commercialSection}>
-				<div className="grid justify-center align-center z-index-1">
-					I will create the website you desire, make it interactive, applying specific 3d models for it in case you want it or 2d animations
-					that will make your website look modern and scalable with the help of these technologies:
-					<div className="flex justify-center align-center">
-						<img src="/testimages/Reactjslogo.svg" alt="reactlogo" className="flex-child white technologies"></img>
-						<img src="/testimages/nodejslogo.svg" alt="nodejs" className="flex-child white technologies"></img>
-						<img src="/testimages/postgresqllogo.svg" alt="postgresql" className="flex-child white technologies"></img>
-						<img src="/testimages/spring_boot.svg" alt="spring boot" className="flex-child white technologies"></img>
-						<img src="/testimages/threejslogo.svg" alt="threejs" className="flex-child white technologies"></img>
+				<div style={{height: "200px"}} className="py-3 z-index-1 grid justify-center align-items-center bold">
+					<div className="flex justify-center align-items-center z-index-1">
+						{welcomeMessage.map((letter, index) => {
+							return <h1 key={index} style={{ '--i': index, fontSize: "50px" }} className="animate-text pl-1 Teko underline">{letter}</h1>
+						})}
 					</div>
+					<p style={{lineHeight: "1.6"}} className="z-index-1 px-2 bold">Contact me through LinkedIn, my e-mail or use the form down below!</p>
 				</div>
 				<Particles div={commercialSection} colorParticles={"white"} />
 			</div>
