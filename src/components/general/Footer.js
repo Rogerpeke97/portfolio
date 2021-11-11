537f07f55ab4cce880cec6883c75a750941bf1d5
Ignacio <rogerpeke97@gmail.com>
2021-11-10 20:48:15 -0500

    Added message copied to clipboard

--- src/components/general/Footer.js
+++ src/components/general/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGoogle } from '@fortawesome/free-brands-svg-icons'
 import { faAddressBook, faInfoCircle, faMapMarked } from '@fortawesome/free-solid-svg-icons'
@@ -6,8 +7,14 @@ import Form from './Form';
 
 function Footer({ mediaQuery }) {
 
+  const [copied, setCopied] = useState({value: false, busy: false});
+
   const copiedToClipboardFn = (textToCopy) => {
-    navigator.clipboard.writeText(textToCopy)
+    if(!copied.busy) {
+      setCopied({value: true, busy: true});
+      navigator.clipboard.writeText(textToCopy)
+      setTimeout(()=>setCopied({value: false, busy: false}),1000)
+    }
   }
 
   return (
@@ -21,7 +28,8 @@ function Footer({ mediaQuery }) {
         </h4>
         <h4>
           <FontAwesomeIcon className="pr-1" icon={faGoogle} /> 
-          <i><span className="underline cursor-pointer" onClick={()=>copiedToClipboardFn("rogerpeke97@gmail.com")}>rogerpeke97@gmail.com</span></i>
+          <i><span className="underline cursor-pointer" 
+          onClick={()=>copiedToClipboardFn("rogerpeke97@gmail.com")}>rogerpeke97@gmail.com</span></i>
         </h4>
         <h4 className="word-break-word">
           &copy; Copyright 2021, Ignacio Martin Diaz. All rights reserved.
@@ -33,6 +41,7 @@ function Footer({ mediaQuery }) {
         </h1>
         <Form />
       </div>
+      {copied.value && <div className="clipboard-message bold button-shadow">Text copied!</div>}
     </footer>
   )
 }
