b74027d3f0042fc925b4a01ec5d7117784d816bc
Ignacio <rogerpeke97@gmail.com>
2021-11-13 18:34:54 -0500

    disabled submit button when not ready

--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -2,10 +2,10 @@ import React from "react";
 import Spinner from "../spinners/Spinner";
 
 
-const Button = ({ onClick, ButtonText, shadow, isLoading }) => {
+const Button = ({ onClick, ButtonText, shadow, isLoading, disabled }) => {
 
  return (
-  <button disabled={isLoading}
+  <button disabled={isLoading || disabled}
    className={`flex-child justify-center align-items-center ${ButtonText ? "icon-button-text" : "icon-button"} px-1 ${shadow ? 'button-shadow' : ''}`}
    onClick={onClick}>
     {isLoading ? <Spinner /> : <span className="text-overflow-ellipsis white-space-nowrap mx-1 bold fade-in">{ButtonText}</span>}
