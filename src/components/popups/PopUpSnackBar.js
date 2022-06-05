e1610108a804ffa09743893995bcd0599cba921d
Ignacio <rogerpeke97@gmail.com>
2022-06-05 01:33:34 -0300

    transition for smoother vh transition

--- src/components/popups/PopUpSnackBar.js
+++ src/components/popups/PopUpSnackBar.js
@@ -28,9 +28,7 @@ const PopUpSnackBar = ({ show, message, icon, onCloseDialog }) => {
     mdAndUp:max-w-[500px] smAndDown:w-full inset-0 bg-secondary items-center m-auto z-50 p-5 flex flex-col font-bold shadow-xl`}>
    <div className="flex w-full items-center justify-center min-width-0 px-2">
     <FontAwesomeIcon className="text-black" icon={icon} />
-     <p className="text-ellipsis ml-2 font-bold text-black truncate px-1" style={{whiteSpace: 'pre-wrap'}}>
-       asdalksdjasdjakjsdjkasjkdakjsdkjasjkdkjasdkjakjsdkjasdkjakjsdjkasdjkasjkdakjsdkjasdkjaskjdakjsdkjaskjdaskjdjkasdkjaskjdas
-     </p>
+     <p className="text-ellipsis ml-2 font-bold text-black truncate px-1" style={{whiteSpace: 'pre-wrap'}}>{message}</p>
    </div>
    <IconButton className="ml-3 w-[150px] mt-auto" type="button" ButtonText={"Ok"} IconName={faCheck} onClick={onCloseDialog} />
   </div>
